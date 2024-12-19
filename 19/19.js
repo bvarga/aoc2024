ec552066580e2cddef3d8ba74c7984b427ea001e
Balazs Varga <bb.varga@gmail.com>
2024-12-19 20:56:36 +0100

    day 19 part 2

--- 19/19.js
+++ 19/19.js
@@ -30,36 +30,59 @@ const data = raw.split("\n").reduce(
   })
 );
 
-const part1 = data.designs?.filter((it) => isPossible(it, "")).length;
-console.log(`Part 1: ${part1}`);
+/** @type {Record<string, number>} */
+const cache = {};
+
+const results = data.designs.reduce(
+  (acc, design) => {
+    const count = process(design);
+    acc.possible += count > 0 ? 1 : 0;
+    acc.sum += count;
+    return acc;
+  },
+  /** @type {{possible: number, sum: number}} */
+  ({
+    possible: 0,
+    sum: 0,
+  })
+);
+
+console.log(`Part 1: ${results.possible}`);
+console.log(`Part 2: ${results.sum}`);
 
 /**
  * @param {string} design
- * @param {string} prev
- * @returns {boolean}
+ * @returns {number}
  */
-function isPossible(design, prev = "") {
-  if (design.length <= data.max && data.towels.includes(design)) {
-    return true;
+function process(design) {
+  if (design.length === 0) {
+    return 0;
   }
 
-  let i = data.max;
+  if (cache[design]) {
+    return cache[design];
+  }
 
-  while (i > 0) {
+  let sum = 0;
+  let i = 1;
+  while (i < data.max && i <= design.length) {
     const starting = design.substring(0, i);
     if (data.towels.includes(starting)) {
-      if (prev !== "") {
-        data.towels.push(prev + starting);
-        data.max = Math.max(data.max, prev.length + starting.length);
+      const remaining = design.substring(i);
+      if (remaining === "") {
+        sum++;
+        i++;
+        continue;
       }
-      if (isPossible(design.substring(i), prev + starting)) {
-        return true;
+      const sub = process(remaining);
+      if (sub === 0) {
+        // return 0;
       } else {
-        i--;
+        sum += sub;
       }
-    } else {
-      i--;
     }
+    i++;
   }
-  return false;
+  cache[design] = sum;
+  return sum;
 }
